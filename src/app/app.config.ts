2745ff87910a8ea7f900768da87cc6c59d4514ae
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2024-08-12 10:43:53 +0300

    Correct husky

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,5 +5,9 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+  ],
 };
